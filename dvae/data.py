847440a2716eb1d3e663e4ad01734f4d8bcb9f19
niksaz <nikitasazanovich@gmail.com>
2021-03-06 21:56:07 +0000

    Chairs dataset added.

--- dvae/data.py
+++ dvae/data.py
@@ -6,6 +6,8 @@ import numpy as np
 import torch
 import torch.utils.data
 from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+from torchvision.datasets import ImageFolder
 
 
 def get_dataloader(config):
@@ -14,13 +16,42 @@ def get_dataloader(config):
   image_size = config['image_size']
   if name == 'dsprites':
     data_path = os.path.join('data', 'dsprites-dataset', 'dsprites_ndarray_co1sh3sc6or40x32y32_64x64.npz')
-    data = np.load(data_path, encoding='bytes')
-    data = torch.from_numpy(data['imgs']).unsqueeze(1).float()
-    dataset = NonTupledTensorDataset(data)
+    dataset = get_non_tupled_tensor_dataset(data_path)
+  elif name == 'chairs':
+    data_path = os.path.join('data', 'Chairs_64')
+    dataset = get_non_tupled_image_folder_dataset(data_path, image_size)
   else:
     raise ValueError(f'Unknown dataset name: {name}.')
   dataloader = DataLoader(dataset, batch_size, shuffle=True, num_workers=2, pin_memory=True, drop_last=True)
   return dataloader
+
+
+def get_non_tupled_image_folder_dataset(data_path, image_size):
+  transform = transforms.Compose([
+    transforms.Resize([image_size, image_size]),
+    transforms.ToTensor(),
+  ])
+  image_folder = ImageFolder(root=data_path, transform=transform)
+  dataset = NonTupledImageFolder(image_folder)
+  return dataset
+
+
+class NonTupledImageFolder(Dataset):
+  def __init__(self, image_folder):
+    self.image_folder = image_folder
+
+  def __getitem__(self, item):
+    return self.image_folder[item][0]
+
+  def __len__(self):
+    return len(self.image_folder)
+
+
+def get_non_tupled_tensor_dataset(data_path):
+  data = np.load(data_path, encoding='bytes')
+  data = torch.from_numpy(data['imgs']).unsqueeze(1).float()
+  dataset = NonTupledTensorDataset(data)
+  return dataset
 
 
 class NonTupledTensorDataset(Dataset):
