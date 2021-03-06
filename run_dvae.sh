d045c4ed784683ad1fa42d404f69f78e09a587a5
niksaz <nikitasazanovich@gmail.com>
2021-03-06 22:24:32 +0000

    Script to run DVAE training on all datasets.

---
+++ run_dvae.sh
@@ -0,0 +1,6 @@
+# Author: Mikita Sazanovich
+
+python main_dvae.py exps/dvae_dsprites.yaml dvae_dsprites
+python main_dvae.py exps/dvae_chairs.yaml dvae_chairs
+python main_dvae.py exps/dvae_cars.yaml dvae_cars
+python main_dvae.py exps/dvae_celeba.yaml dvae_celeba
