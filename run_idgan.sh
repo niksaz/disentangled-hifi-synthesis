e3b89de6b545580b2f66839a1058152f4a4c3c94
niksaz <nikitasazanovich@gmail.com>
2021-03-14 20:22:39 +0000

    Script for ID-GAN experiments.

--- run_idgan.sh
+++ run_idgan.sh
@@ -1,3 +1,6 @@
 # Author: Mikita Sazanovich
 
 python main_idgan.py exps/idgan_dsprites.yaml idgan_dsprites
+python main_idgan.py exps/idgan_chairs.yaml idgan_chairs
+python main_idgan.py exps/idgan_cars.yaml idgan_cars
+python main_idgan.py exps/idgan_celeba.yaml idgan_celeba
