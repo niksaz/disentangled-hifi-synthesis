140512c9d6246a8249e4b133bd1bf5dbfea5edfc
niksaz <nikitasazanovich@gmail.com>
2021-03-22 20:54:45 +0000

    Updated the script to rub Celeba 128 and 256 too.

--- run_idgan.sh
+++ run_idgan.sh
@@ -4,3 +4,5 @@ python main_idgan.py exps/idgan_dsprites.yaml idgan_dsprites
 python main_idgan.py exps/idgan_chairs.yaml idgan_chairs
 python main_idgan.py exps/idgan_cars.yaml idgan_cars
 python main_idgan.py exps/idgan_celeba.yaml idgan_celeba
+python main_idgan.py exps/idgan_celeba_128.yaml idgan_celeba_128
+python main_idgan.py exps/idgan_celeba_256.yaml idgan_celeba_256
