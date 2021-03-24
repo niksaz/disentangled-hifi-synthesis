cda7a3af947ff9acdc2a33aea61c04cc37e0d48e
niksaz <nikitasazanovich@gmail.com>
2021-03-24 20:37:10 +0000

    Celeba 1024 added to the training script.

--- run_idgan.sh
+++ run_idgan.sh
@@ -6,3 +6,4 @@ python main_idgan.py exps/idgan_cars.yaml idgan_cars
 python main_idgan.py exps/idgan_celeba.yaml idgan_celeba
 python main_idgan.py exps/idgan_celeba_128.yaml idgan_celeba_128
 python main_idgan.py exps/idgan_celeba_256.yaml idgan_celeba_256
+python main_idgan.py exps/idgan_celeba_1024.yaml idgan_celeba_1024
